bb997ff1462e45f9a3bccfb9da9bb30976a0a820
Maximilian Kranabetter <maximilian.kranabetter@student.uibk.ac.at>
2018-04-24 20:53:29 +0200

    added subdomains and attribution to osm and deactivated imperial scale

--- basemap.js
+++ basemap.js
@@ -2,7 +2,10 @@
 let myMap = L.map("mapdiv");            // http://leafletjs.com/reference-1.3.0.html#map-l-map
 let myLayers = {
     osm : L.tileLayer(                  // http://leafletjs.com/reference-1.3.0.html#tilelayer-l-tilelayer
-        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"            // bis jetzt keinen weiteren Kartendienst gefunden, um Objekt hinzuzufügen
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            subdomains : ["a", "b", "c"],   // habe Laura und Michaela gefragt, wie das geht :)
+            attribution : "Datenquelle: <a href='https://www.openstreetmap.org'>© OpenStreetMap-Mitwirkende</a>"
+        }
     ),
     geolandbasemap : L.tileLayer(
         "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
@@ -41,12 +44,12 @@ myMap.addLayer(myLayers.bmaporthofoto30cm);         // http://leafletjs.com/refe
 
 let myMapControl = L.control.layers({               // http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
     "Openstreetmap" : myLayers.osm,
-    "basemap.at Grundkarte" : myLayers.geolandbasemap,
-    "basemap.at grau" : myLayers.bmapgrau,
-    "basemap.at highdpi" : myLayers.bmaphidpi,
-    "basemap.at Orthofoto" : myLayers.bmaporthofoto30cm
+    "Basemap Grundkarte" : myLayers.geolandbasemap,
+    "Basemap grau" : myLayers.bmapgrau,
+    "Basemap highdpi" : myLayers.bmaphidpi,
+    "Basemap Orthofoto 30cm" : myLayers.bmaporthofoto30cm
 },{
-    "basemap.at Overlay" : myLayers.bmapoverlay
+    "Basemap Overlay" : myLayers.bmapoverlay
 },{
     collapsed: false // http://leafletjs.com/reference-1.3.0.html#control-layers-collapsed
 });
@@ -56,6 +59,7 @@ myMap.addControl(myMapControl);
 L.control.scale({               // http://leafletjs.com/reference-1.3.0.html#control-scale-l-control-scale
     position: "bottomleft",     // http://leafletjs.com/reference-1.3.0.html#control-scale-position
     metric: true,               // http://leafletjs.com/reference-1.3.0.html#control-scale-metric
+    imperial: false,            // http://leafletjs.com/reference-1.3.0.html#control-scale-imperial
     maxWidth: 200,              // http://leafletjs.com/reference-1.3.0.html#control-scale-maxwidth
 }).addTo(myMap);                // http://leafletjs.com/reference-1.3.0.html#control-scale-addto
 
