311112159798fcb7331acc3446bd7b32ce339367
Maximilian Kranabetter <maximilian.kranabetter@student.uibk.ac.at>
2018-04-25 18:13:27 +0200

    added patschfoto to library and made a popup

--- marker.js
+++ marker.js
@@ -1,5 +1,6 @@
 
 let myMap = L.map("mapdiv");            // http://leafletjs.com/reference-1.3.0.html#map-l-map
+let markerGroup = L.featureGroup();
 
 let myLayers = {
     osm : L.tileLayer(                  // http://leafletjs.com/reference-1.3.0.html#tilelayer-l-tilelayer
@@ -40,7 +41,7 @@ let myLayers = {
     ),
 };
 
-myMap.addLayer(myLayers.bmaporthofoto30cm);         // http://leafletjs.com/reference-1.3.0.html#map-addlayer
+myMap.addLayer(myLayers.geolandbasemap);         // http://leafletjs.com/reference-1.3.0.html#map-addlayer
 
 let myMapControl = L.control.layers({               // http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
     "Openstreetmap" : myLayers.osm,
@@ -49,7 +50,8 @@ let myMapControl = L.control.layers({               // http://leafletjs.com/refe
     "Basemap highdpi" : myLayers.bmaphidpi,
     "Basemap Orthofoto 30cm" : myLayers.bmaporthofoto30cm
 },{
-    "Basemap Overlay" : myLayers.bmapoverlay
+    "Basemap Overlay" : myLayers.bmapoverlay,
+    "Marker" : markerGroup
 },{
     collapsed: false // http://leafletjs.com/reference-1.3.0.html#control-layers-collapsed
 });
@@ -69,18 +71,32 @@ myMap.setView([47.267,11.383], 11);
 const uni = [47.264,11.385];
 const usi = [47.257,11.356];
 const technik = [47.263,11.343];
+const igls = [47.232,11.408];
+const patscherkofel = [47.209,11.461];
 
-let markerGroup = L.featureGroup();
 myMap.addLayer(markerGroup);
 
 const markerOptions = {
     title: "Universität Innsbruck",
     opacity: 0.6,
     draggable: true
-}
+};
+
+const markerOption2 = {
+    title: "Igls Ortschaft"
+};
+
+const markerOption3 = {
+    title: "Patscherkofel"
+};
 
 L.marker(uni, markerOptions).addTo(markerGroup);
 L.marker(usi, markerOptions).addTo(markerGroup);
 L.marker(technik, markerOptions).addTo(markerGroup);
+
+L.marker(igls,markerOption2).addTo(markerGroup);
+
+L.marker(patscherkofel,markerOption3).addTo(markerGroup).bindPopup("<p>Ausblick vom Patscherkofel</p><img style= 'width:160px' src='js/leaflet/images/patschfoto.jpg' alt='Patscherkofel'/>");
+
 
 myMap.fitBounds(markerGroup.getBounds());
