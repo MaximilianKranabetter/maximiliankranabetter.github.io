39a4e4b023e9fbfb76565ea80211b47e7c8b6a08
Maximilian Kranabetter <maximilian.kranabetter@student.uibk.ac.at>
2018-05-19 17:23:18 +0200

    layer und massstab, bisher orientiert an lisa428 und valerieoswald

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -1,3 +1,83 @@
+let myMap = L.map("map", {
+    fullscreenControl: true //Kontrolloption, um Karte als Fullscreen darzustellen
+});
+
+let etappe19Group = L.featureGroup().addTo(myMap);
+
+let myLayers = {
+
+    osm: L.tileLayer( 
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            subdomains: ['a', 'b', 'c'],
+            attribution: "Datenquelle: <a href = 'https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+        }
+    ),
+    geolandbasemap: L.tileLayer(
+        'https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png', {
+            subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"], 
+            attribution: "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>" 
+        }
+    ),
+    gdi_summer: L.tileLayer(
+        "http://wmts.kartetirol.at/wmts/gdi_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+            minZoom: 0,
+            maxZoom: 18,    
+            attribution: "Datenquelle: <a href = 'https://www.tirol.gv.at/statistik-budget/tiris/tiris-geodatendienste/impressum-elektronische-karte-tirol/'>Elektronische Karte Tirol</a>"
+        }
+    ),
+    gdi_winter: L.tileLayer(
+        "http://wmts.kartetirol.at/wmts/gdi_winter/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+            minZoom: 0,
+            maxZoom: 18,     
+            attribution: "Datenquelle: <a href = 'https://www.tirol.gv.at/statistik-budget/tiris/tiris-geodatendienste/impressum-elektronische-karte-tirol/'>Elektronische Karte Tirol</a>"
+        }
+    ),
+    gdi_ortho: L.tileLayer(
+        "http://wmts.kartetirol.at/wmts/gdi_ortho/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+            minZoom: 0,
+            maxZoom: 18,     
+            attribution: "Datenquelle: <a href = 'https://www.tirol.gv.at/statistik-budget/tiris/tiris-geodatendienste/impressum-elektronische-karte-tirol/'>Elektronische Karte Tirol</a>"
+        }
+    ),
+
+    bmapoverlay: L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+            attribution: "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>"
+        }
+    ),
+
+};
+
+myMap.addLayer(myLayers.geolandbasemap); //Die Geolandbasemap als Default anzeigen lassen
+
+myMap.addLayer(etappe19Group);
+
+let myMapControl = L.control.layers({   //Kontrollmenü für die Layer
+    "Openstreetmap": myLayers.osm,
+    "basemap.at Grundkarte": myLayers.geolandbasemap,
+    "Elektronische Karte Tirol Sommer": myLayers.gdi_summer,
+    "Elektronische Karte Tirol Winter": myLayers.gdi_winter,
+    "Elektronische Karte Tirol Orthofoto": myLayers.gdi_ortho
+}, {
+    "basemap.at Overlay": myLayers.bmapoverlay,
+    "Etappe 19: ": etappe19Group,   //feature Group dem Overlay-Layer hinzufügen
+    //'Group: ': marker,
+}, {
+    collapsed: true //das Menü nicht automatisch aufmachen
+
+}).addTo(myMap);
+
+//Maßstab
+let mayScale = L.control.scale({
+    maxWidth: 200,
+    metric: true,
+    imperial: false,
+}).addTo(myMap);
+
+const start = [47.162648,11.745063];
+const finish = [47.298682,11.666158];
+
 /*
     Vorbereitung: GPX Track herunterladen und nach GeoJSON konvertieren
     -------------------------------------------------------------------
@@ -11,12 +91,12 @@
     -> statt 00 natürlich die eigene Etappe (z.B. 01,02, ...25)
 */
 
-// eine neue Leaflet Karte definieren
+// :) eine neue Leaflet Karte definieren
 
-// Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
-// WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
+// :) Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
+// :) WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
 
-// Maßstab metrisch ohne inch
+// :) Maßstab metrisch ohne inch
 
 // Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
 
