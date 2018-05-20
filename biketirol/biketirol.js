9a5e1a6e10fe9da557aa00599d26af4a952910fd
Maximilian Kranabetter <maximilian.kranabetter@student.uibk.ac.at>
2018-05-20 18:46:58 +0200

    added scripts and completed index html, modified gpxTrack code

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -109,8 +109,8 @@ L.control.scale({
 L.marker([47.162648,11.745063],{
     icon : L.icon({
         iconUrl : "images/start.png",
-        iconAnchor : [24,48],
-        popupAnchor : [0,-48],
+        iconAnchor : [16,37],
+        popupAnchor : [0,-37],
     })
 }).bindPopup(
     "<h3>Lanersbach (Gemeinde Tux)</h3><p><a href='https://de.wikipedia.org/wiki/Tux_(Tirol)#Lanersbach'>Wikipedia Link</a></p>"
@@ -119,8 +119,8 @@ L.marker([47.162648,11.745063],{
 L.marker([47.298682,11.666158],{
     icon : L.icon({
         iconUrl : "images/finish.png",
-        iconAnchor : [24,48],
-        popupAnchor : [0,-48],
+        iconAnchor : [16,37],
+        popupAnchor : [0,-37],
     })
 }).bindPopup(
     "<h3>Weerberg</h3><p><a href='https://de.wikipedia.org/wiki/Weerberg'>Wikipedia Link</a></p>"
@@ -128,47 +128,32 @@ L.marker([47.298682,11.666158],{
 
 // GPX Track direkt laden und auf Ausschnitt zoomen
 
-let gpxTrack = new L.GPX("data/etappe19.gpx", { //wieso Fehlermeldung L.GPX is not a constructor
+let gpxTrack = new L.GPX("data/etappe19.gpx", {
     async: true
 }).addTo(overlayTrack);
 gpxTrack.on('loaded', function(evt) {
     let track = evt.target;
-    console.log("get_distance",       track.get_distance().toFixed(0))
-    console.log("get_elevation_min",  track.get_elevation_min().toFixed(0))
-    console.log("get_elevation_max",  track.get_elevation_max().toFixed(0))
-    console.log("get_elevation_gain", track.get_elevation_gain().toFixed(0))
-    console.log("get_elevation_loss", track.get_elevation_loss().toFixed(0))
+
+    let laenge = evt.target.get_distance().toFixed(0);
+    document.getElementById("laenge").innerHTML=laenge;
+    // console.log("get_distance",       track.get_distance().toFixed(0))
+
+    let elevmin = evt.target.get_elevation_min().toFixed(0);
+    document.getElementById("elevmin").innerHTML=elevmin;
+    // console.log("get_elevation_min",  track.get_elevation_min().toFixed(0))
+
+    let elevmax = evt.target.get_elevation_max().toFixed(0);
+    document.getElementById("elevmax").innerHTML=elevmax;
+    // console.log("get_elevation_max",  track.get_elevation_max().toFixed(0))
+
+    let elevgain = evt.target.get_elevation_gain().toFixed(0);
+    document.getElementById("elevgain").innerHTML=elevgain;
+    // console.log("get_elevation_gain", track.get_elevation_gain().toFixed(0))
+
+    let elevloss = evt.target.get_elevation_loss().toFixed(0);
+    document.getElementById("elevloss").innerHTML=elevloss;
+    // console.log("get_elevation_loss", track.get_elevation_loss().toFixed(0))
+
     myMap.fitBounds(track.getBounds());
 
-    document.getElementById("get_distance").innerHTML = track.get_distance().toFixed(0);
 });
-
-
-/*
-    Vorbereitung: GPX Track herunterladen und nach GeoJSON konvertieren
-    -------------------------------------------------------------------
-    :) Datenquelle https://www.data.gv.at/suche/?search-term=bike+trail+tirol&searchIn=catalog
-    :) Download Einzeletappen / Zur Ressource ...
-    :) Alle Dateien im unterverzeichnis data/ ablegen
-    :) Die .gpx Datei der eigenen Etappe als etappe00.gpx speichern
-    :) Die .gpx Datei über https://mapbox.github.io/togeojson/ in .geojson umwandeln und als etappe00.geojson speichern
-    :) Die etappe00.geojson Datei in ein Javascript Objekt umwandeln und als etappe00.geojson.js speichern
-
-    -> statt 00 natürlich die eigene Etappe (z.B. 01,02, ...25)
-*/
-
-// :) eine neue Leaflet Karte definieren
-
-// :) Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
-// :) WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
-
-// :) Maßstab metrisch ohne inch
-
-// Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
-
-// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
-// Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
-
-// Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
-
-// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
